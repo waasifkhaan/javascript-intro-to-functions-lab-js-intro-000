9380f87ca3fc4d79acdb97dd15499f0bf0119a1f
Wasif Khan <waasif.khaan@gmail.com>
2020-03-21 21:31:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,4 +19,11 @@ function sayHiToGrandma(string){
   if(temp.toLowerCase() === string){
     return "I can't hear you!"
   }
+  else if (temp.toUpperCase() === string)
+  {
+    return "YES INDEED!"
+  }
+  else {
+    return "I love you too."
+  }
 }
